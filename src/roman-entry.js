05c7598805eed8fd747fb0afef816b1f23bd3232
Trina <trina.shih@gmail.com>
2026-08-30 15:44:10 -0700

    Default launch-day site to Roman Mission mode

---
+++ src/roman-entry.js
@@ -0,0 +1,5 @@
+import './roman-mission.js';
+
+// Launch day is the commemorative default. Importing roman-mission.js first
+// guarantees all controls/listeners exist before the mode switch is triggered.
+document.getElementById('modeRoman')?.click();
