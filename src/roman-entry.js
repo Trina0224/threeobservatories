1dc0eab5b1e194cd3cb7c99fedce14b8d7c4c789
Trina <trina.shih@gmail.com>
2026-08-30 15:58:29 -0700

    bust Roman renderer module cache

--- src/roman-entry.js
+++ src/roman-entry.js
@@ -1,5 +1,5 @@
-import './roman-mission.js';
-import './roman-heliocentric.js';
+import './roman-mission.js?v=20260830k';
+import './roman-heliocentric.js?v=20260830m';
 
 // Launch day is the commemorative default. Import both renderers first so all
 // camera controls and frame switches exist before Roman Mission is activated.
